7f4a1923142e1279a535b7448ff0669e0e081953
cleverton.decker <Cleverton_decker@hotmail.com>
2020-03-06 17:42:47 -0300

    primeiro commit da parte de query

---
+++ src/main/java/br/com/totvs/sistemaescolar/query/AluraSpringBootParte2Admin1Application.java
@@ -0,0 +1,13 @@
+package br.com.totvs.sistemaescolar.query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraSpringBootParte2Admin1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraSpringBootParte2Admin1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sistemaEscola/AluraSpringBootParte2Admin1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sistemaEscola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraSpringBootParte2Admin1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
