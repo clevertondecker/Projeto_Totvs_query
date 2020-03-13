d6a08e93955c2b75b8149d1a7d90bf95140b6d7e
cleverton.decker <Cleverton_decker@hotmail.com>
2020-03-13 08:09:49 -0300

    Correcoes flyway

--- src/main/resources/db/migration/V1.0__initialize_schema.sql
+++ src/main/resources/db/migration/V1.0__initialize_schema.sql
@@ -1,5 +1,5 @@
 
-CREATE TABLE IF NOT EXISTS "aluno"
+CREATE TABLE  aluno
 (
     id character varying(255) COLLATE pg_catalog."default" NOT NULL,
     nome character varying(255) COLLATE pg_catalog."default" NOT NULL,
@@ -11,14 +11,13 @@ CREATE TABLE IF NOT EXISTS "aluno"
     CONSTRAINT aluno_pkey PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS "professor"
+CREATE TABLE professor
 (
     id character varying(255) COLLATE pg_catalog."default" NOT NULL,
     nome character varying(255) COLLATE pg_catalog."default" NOT NULL,
     email character varying(255) COLLATE pg_catalog."default" NOT NULL,
     cpf character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    matricula character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    forma_Ingresso character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    turma_Id character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT aluno_pkey PRIMARY KEY (id)
+    titulo character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    disciplina_Id character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT professor_pkey PRIMARY KEY (id)
 );
